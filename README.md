4bd56348f032cd3e7d557453ac5502230ee5acfa
adampats <adampatterson@protonmail.com>
2017-12-11 17:14:15 -0800

    WIP

--- README.md
+++ README.md
@@ -1,19 +1,31 @@
 # coin_watcher
 Monitor and alert on cryptocurrency exchange rates via Lambda
 
-# Development
+Built using [python-lambda](https://github.com/nficano/python-lambda).
 
-How to setup a dev environment for this:
+## Run it local
+
+How to setup a dev environment + run this:
 
 ```sh
 pip install virtualenv
 virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
+export SNS_TOPIC_ARN='arn:aws:sns:us-west-2:111111111111:example'
+lambda invoke
 ```
+
+Setting the `SNS_TOPIC_ARN` env var is optional.  Leave it unset to just run in "local" mode.
 
 coinmarketcap API example:
 
 ```sh
 curl -s -k "https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=USD" | jq .
+```
+
+## Run it on AWS Lambda
+
+```sh
+lambda deploy
 ```
---
+++ main.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+import requests
+import time
+import os
+import json
+import boto3
+from botocore.exceptions import ClientError
+
+from IPython import embed
+
+BASE_API_URI = 'https://api.coinmarketcap.com/v1/'
+HEADERS = { 'Accept': 'application/json',
+            'Content-Type': 'application/json' }
+VERBOSE = True
+MSG_SUBJECT = 'coin_watcher alert'
+
+def get_data():
+    r = requests.get(BASE_API_URI + "ticker", headers=HEADERS)
+    return r.json()
+
+def send_sns(topic_arn, msg, subject):
+    try:
+        client = boto3.client('sns')
+        arn = client.get_topic_attributes(TopicArn = topic_arn)
+        resp = client.publish(
+            TopicArn = topic_arn,
+            Subject = subject,
+            Message = msg)
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'NotFound':
+            print("SNS Topic,", topic_arn, "not found.  Message not sent.")
+        else:
+            print("Unexpected error:", e)
+
+# main
+def handler(event, context):
+    verbose = VERBOSE
+    checks = event.get('checks')
+    alert_email = event.get('alert_email')
+    if 'SNS_TOPIC_ARN' in os.environ:
+        sns_topic_arn = os.environ['SNS_TOPIC_ARN']
+    else:
+        sns_topic_arn = 'local'
+
+    if checks:
+        print("Querying...")
+        data = get_data()
+
+        alerts = []
+        alert_subject = ""
+        for c in checks:
+            cur = next(item for item in data if item['symbol'] == c['coin'])
+
+            cur = {k: cur.get(k, None) for k in (
+                'id', 'symbol', 'price_usd', 'price_usd', 'percent_change_1h',
+                'percent_change_24h', 'percent_change_7d', 'last_updated')}
+            cur['last_updated'] = time.strftime(
+                '%Y-%m-%d %H:%M:%S', time.localtime(int(cur['last_updated'])) )
+
+            if verbose: print("=", cur['symbol'], "=\n", cur)
+
+            if abs(float(cur['percent_change_1h'])) >= float(c['pct_threshold_1h']):
+                cur['reason'] = '1h'
+                alerts.append(cur)
+                if verbose: print(cur['symbol'], "1h threshold triggered!")
+
+            if abs(float(cur['percent_change_24h'])) >= float(c['pct_threshold_24h']):
+                cur['reason'] = '24h'
+                alerts.append(cur)
+                if verbose: print(cur['symbol'], "24h threshold triggered!")
+
+        if alerts != []:
+            alert_subject = " - " + alerts[0]['symbol'] + " - " + alerts[0]['reason']
+        sns_msg = json.dumps(alerts, indent=4, sort_keys=True)
+        if verbose:
+            print("Alerts", alert_subject, "\n", sns_msg)
+        if sns_topic_arn == 'local':
+            print("Skipping SNS publish since SNS_TOPIC_ARN environment " + \
+                  "variable is not specified.")
+        else:
+            send_sns(sns_topic_arn, sns_msg, MSG_SUBJECT + alert_subject)
+
+    else:
+        print("No checks defined.  Quit.")
